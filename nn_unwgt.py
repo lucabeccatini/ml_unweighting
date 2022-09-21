07ccf8cbd884613205ae1a8229530ee03cd049b2
lucabeccatini <lb_linux@VictusLB.localdomain>
2022-09-21 10:05:53 +0200

    added unweighting 1, 2 and efficiencies

--- nn_unwgt.py
+++ nn_unwgt.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # # # # # unwgting class ???
 
-# # # # # implement for negative wgt
+# # # # # better to use pandas to record all the info ???
 
 
 ##################################################
@@ -27,6 +27,7 @@ with open('/home/lb_linux/nn_unwgt/info_wgt_events.txt', 'r') as infof:
     wgt_train, wgt_val = data[:-1000, -1], data[-1000:, -1]
 
 # define the model
+tf.random.set_seed(1) 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(16, activation='relu', input_shape = (6,)),
     tf.keras.layers.Dense(16, activation='relu'), 
@@ -38,7 +39,8 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 
 # training and test
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=30)
-history = model.fit(X_train, wgt_train, validation_data=(X_val, wgt_val), batch_size=1000, epochs=100, callbacks=[callback]) 
+history = model.fit(X_train, wgt_train, validation_data=(X_val, wgt_val), batch_size=1000, epochs=1, callbacks=[callback]) 
+"""
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
@@ -46,6 +48,7 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['Train', 'Validation'], loc='best')
 plt.show()
+"""
 
 
 
@@ -54,29 +57,71 @@ plt.show()
 ##################################################
 
 # definitions of maximum functions
-def max_quantile_reduction(arr):
-    arr = np.sort(arr)
-    tot_sum = np.sum(arr)
+def max_quantile_reduction(array_s, array_x=[]):
     part_sum = 0 
-    rem_cont = 0.001                                       # coefficient of the remaining contribution to the total sum
-    for i in len(arr):
-        part_sum += arr[i]
-        if (part_sum > tot_sum*(1-rem_cont)):
-            max = arr[i]
-            break
-    return max
+    r = 0.001                             # coefficient of the remaining contribution to the total sum
+    
+    if (len(array_x) == 0):                      # max for s_i
+        arr_s = np.sort(array_s)
+        tot_sum = np.sum(arr_s)
+        for i in range(len(arr_s)):
+            part_sum += arr_s[i]
+            if (part_sum >= tot_sum*(1-r)):
+                max_s = arr_s[i]
+                break
+        return max_s
+    
+    else:                                          # max for x_i
+        arr_s = [s for _,s in sorted(zip(array_x, array_s))]         # sort s_i respect to x_i for the second unwgt
+        arr_x = np.sort(array_x)
+        tot_sum = np.sum(arr_s*arr_x)
+        for i in range(len(arr_s)):
+            part_sum += arr_s[i]*arr_x[i]
+            if (part_sum >= tot_sum*(1-r)):
+                max_x = arr_x[i]
+                break
+        return max_x
 
 # # # def max_median_reduction(arr):
 
 my_max = max_quantile_reduction
 
-np.random.seed(7)
+np.random.seed(1)
 
 # first unweighting
 s1 = model.predict(X_val)
-s1_max = my_max(s1)
-rand1 = np.random.rand(len(s1))
-s2 = []
-for i in range(len(s1)):
-    if (s1[i]/s1_max > rand1[i]):
+s_max = my_max(s1)
+rand1 = np.random.rand(len(s1))            
+w2_true = np.empty(0)                            # real wgt evaluated after first unwgt
+s2 = np.empty(0)                                 # predicted wgt kept by first unwgt
+for i in range(len(s1)):                         # first unweighting, based on the predicted wgt
+    if (s1[i]/s_max > rand1[i]):
+        s2 = np.append(s2, s1[i])
+        w2_true = np.append(w2_true, wgt_val[i])
+        # w2 = np.append(w2, max(1, s1[i]/s_max))
+x2 = np.divide(w2_true, s2)
 
+# second unweighting, based on the ratio x
+x_max = my_max(s2, x2)
+rand2 = np.random.rand(len(x2)) 
+s3 = np.empty(0)                                 # wgt of kept event after second unweighting
+x3 = np.empty(0)
+for i in range(len(s2)):                         # second unweighting, based on the ratio x
+    if ((x2[i]*max(1, s2[i]/s_max)/x_max) > rand2[i]):
+        s3 = np.append(s3, s2[i])
+        x3 = np.append(x3, x2[i])
+        # w3 = np.append(w3, max(1, x[i]*max(1, s2[i]/s_max)/x_max)) 
+
+# efficiencies of the unweightings
+def efficiency_1(s1, s2, s_max):
+    eff = np.sum(np.maximum(1, s2/s_max))**2 * np.sum(s1) / (len(s2) * np.sum(np.maximum(1, s2/s_max)**2) * len(s1) * s_max)
+    return eff 
+
+def efficiency_2(x2, x3, x_max, s3, s_max):
+    eff = np.sum(np.maximum(1, x3*np.maximum(1, s3/s_max)/x_max))**2 * np.sum(x2) / (len(x3) * np.sum(np.maximum(1, x3*np.maximum(1, s3/s_max)/x_max)**2) * len(x2) * x_max)
+    return eff
+
+eff1 = efficiency_1(s1, s2, s_max)
+eff2 = efficiency_2(x2, x3, x_max, s3, s_max)
+print(eff1)
+print(eff2)
