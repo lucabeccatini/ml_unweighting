462ccd7c1b96664e7d1c1ed5bcd71ed7905fa1dd
lucabeccatini <lb_linux@VictusLB.localdomain>
2022-10-07 11:52:09 +0200

    Added chisquare loss, effective gain factor and double loop over r parameter of max

--- nn_unwgt.py
+++ nn_unwgt.py
@@ -7,14 +7,42 @@ import matplotlib.pyplot as plt
 # settings
 ##################################################
 seed_all = 2
-norm = "s_prot"                                  # s_prot or s_int
+norm = "s_pro"                                   # s_pro or s_int
+output = "wgt"                                   # wgt or lnw
+lossfunc = "mse"                                 # mse or chi
 maxfunc = "mqr"                                  # mqr or mmr
-output = "w"                                     # w or logw
+
+eff1_st = 0.099                                  # standard effeciencies for the first unwgt
+eff2_st = 0.997                                  # standard effeciencies for the second unwgt
+
+
+
+# efficiencies functions
+def f_kish(w):                                   # kish factor
+    if (len(w)==0):
+        return 0
+    res = np.sum(w)**2 / (len(w) * np.sum(w**2))
+    return res
+
+def efficiency_1(s1, s2, s_max):                 # efficiency of the first unwgt
+    eff = f_kish(np.sign(s2)*np.maximum(1, np.abs(s2)/s_max)) * np.sum(s1) / (len(s1) * s_max)
+    return eff 
+
+def efficiency_2(x2, x3, x_max, s3, s_max):      # efficiency of the second unwgt
+    eff = f_kish(np.sign(s3)*np.maximum(1, x3*np.maximum(1, np.abs(s3)/s_max)/x_max)) * np.sum(x2) / (len(x2) * x_max)
+    return eff
+
+
+def f_eff(s3 ,x3, s_max, x_max, eff1, eff2):                           # effective gain factor
+    t_ratio = 0.02       # t_surrogate / t_sstandard  [1/20, 1/50, 1/100, 1/500]
+    fkish2 = f_kish(np.sign(s3)*np.maximum(1, x3*np.maximum(1, np.abs(s3)/s_max)/x_max))
+    res = fkish2 / (t_ratio*(eff1_st*eff2_st)/(eff1*eff2) + (eff1_st*eff2_st/eff2))
+    return res
 
 
 
 ##################################################
-# evaluate the weight of events
+# prediction of weights
 ##################################################
 
 # reading the momenta and weight of events
@@ -30,12 +58,13 @@ with open('/home/lb_linux/nn_unwgt/info_wgt_events_5iter.txt', 'r') as infof:
     X_train, X_val = data[:-4000, :-1], data[-4000:, :-1]
     wgt_train, wgt_val = data[:-4000, -1], data[-4000:, -1]
 
+
+# input normalization
 def energy_cm(X):
     res = np.sqrt((X[3]+X[5])**2 - X[0]**2 - X[1]**2 - (X[2]+X[4])**2)
     return res
 
-# input normalization
-if (norm=="s_prot"):
+if (norm=="s_pro"):
     norm_const = 13000                           # normalization costant equal to s of proton
     X_train, X_val = X_train/norm_const, X_val/norm_const
 if (norm=="s_int"):
@@ -46,6 +75,7 @@ if (norm=="s_int"):
             s_val = energy_cm(X_val[i])
             X_val[i] /= s_val
 
+
 # define the model
 tf.random.set_seed(seed_all) 
 model = tf.keras.Sequential([
@@ -54,14 +84,26 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(1)
     ])
 
+
 # loss function and compile the model 
-model.compile(optimizer='adam', loss='mean_squared_error')
+def chi_sqaure(w_true, w_pred):
+    chisq = 0
+    for i in range(len(w_true)):
+        chisq += (w_pred - w_true)**2 / w_true
+    return chisq
+
+if (lossfunc=="mse"):
+    loss = 'mean_squared_error'
+if (lossfunc=="chi"):
+    loss = chi_sqaure
+model.compile(optimizer='adam', loss=loss)   # metrics=[f_eff]
+
 
 # training and test
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=30)
-if (output=="w"):
+if (output=="wgt"):
     history = model.fit(X_train, wgt_train, validation_data=(X_val, wgt_val), batch_size=1000, epochs=100, callbacks=[callback])         # predict w
-if (output=="logw"):
+if (output=="lnw"):
     history = model.fit(X_train, np.abs(np.log(wgt_train)), validation_data=(X_val, np.abs(np.log(wgt_val))), batch_size=1000, epochs=100, callbacks=[callback])         # predict the abs(log(w))
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -69,7 +111,7 @@ plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['Train', 'Validation'], loc='best')
-plt.savefig("/home/lb_linux/nn_unwgt/plot_train_16_16_{}_{}_seed{}.pdf".format(norm, output, seed_all), format='pdf')
+plt.savefig("/home/lb_linux/nn_unwgt/plot_16_16_{}_{}_{}_seed{}_train.pdf".format(norm, lossfunc, output, seed_all), format='pdf')
 
 
 
@@ -77,31 +119,30 @@ plt.savefig("/home/lb_linux/nn_unwgt/plot_train_16_16_{}_{}_seed{}.pdf".format(n
 # preparation of plots
 ##################################################
 
-fig_eff, axs_eff = plt.subplots(2, figsize=(8, 6))  
+fig_eff, axs_eff = plt.subplots(3, figsize=(8, 10))  
 axs_eff[0].set(xlabel="s_max", ylabel="eff_1")
 axs_eff[1].set(xlabel="x_max", ylabel="eff_2")
-fig_eff.legend(title = """Layers: 6, 16, 16, 1 \nEpochs: 100 \nBatch size: 1000 \nEv_train: 12000 \nEv_val: 4000 
-                            \nMax func: {} \nNormalization: {} \nSeed tf and np: {} \n Output: {}""".format(maxfunc, norm, seed_all, output))
+#axs_eff[2].set(xlabel="s_max", ylabel="x_max")
+plot_legend = """Layers: 6, 16, 16, 1 \nEpochs: 100 \nBatch size: 1000 \nEv_train: 12000 \nEv_val: 4000 
+Normalization: {} \nLoss: {} \nOutput: {} \nMax func: {} \nSeed tf and np: {}""".format(norm, lossfunc, output, maxfunc, seed_all)
+fig_eff.legend(title = plot_legend)
 
-fig_ws, axs_ws = plt.subplots(3, figsize=(12, 6))
+fig_ws, axs_ws = plt.subplots(3, figsize=(8, 9))
 axs_ws[0].set(xlabel="w/s", ylabel="dN/d(w/s)")
 axs_ws[1].set(xlabel="w/s", ylabel="dN/d(w/s)")
+axs_eff[2].set_title(label="f_eff")
 axs_ws[2].set(xlabel="w", ylabel="w/s")
-fig_ws.legend(title = """Layers: 6, 16, 16, 1 \nEpochs: 100 \nBatch size: 1000 \nEv_train: 12000 \nEv_val: 4000 
-                            \nMax func: {} \nNormalization: {} \nSeed tf and np: {} \n Output: {}""".format(maxfunc, norm, seed_all, output))
-
-arr_eff1, arr_eff2, arr_smax, arr_xmax = [], [], [], []
+fig_ws.legend(title = plot_legend)
 
 
 
 ##################################################
-# definitions of maximum functions
+# definitions of maxima functions
 ##################################################
 
 def max_quantile_reduction(array_s, array_x=[]):
     # define a reduced maximum such that the overweights' remaining contribution to the total sum of weights is lower or equal to r*total sum
     part_sum = 0 
-    r = arr_r[i_r]                               # fraction of the remaining contribution of larger wgts to the total sum
     if (len(array_x) == 0):                      # max for s_i
         max_s = 0
         if (r <= 0):                             # to test overwgt maxima
@@ -156,11 +197,14 @@ def max_median_reduction(arr):
 
 if (maxfunc=="mqr"): 
     my_max = max_quantile_reduction
-    arr_r = [0.1, 0.01, 0.001, 0.0001, 0, -0.5, -1] 
+    arr_r = [0.1, 0.01, 0.001, 0.0001, 0, -1, -9] 
 
 if (maxfunc=="mmr"):
     my_max = max_median_reduction
-    arr_r = [100, 50, 10, 5, 2, 0, -0.5, -1]
+    arr_r = [100, 50, 10, 5, 0, -1, -9]
+
+arr_eff1, arr_smax = np.empty(len(arr_r)), np.empty(len(arr_r))
+mtx_xmax, mtx_eff2, mtx_feff = np.empty((len(arr_r), len(arr_r))), np.empty((len(arr_r), len(arr_r))), np.empty((len(arr_r), len(arr_r)))
 
 
 
@@ -168,37 +212,16 @@ if (maxfunc=="mmr"):
 # unweighting
 ##################################################
 
-# efficiencies functions
-def f_kish(w):                         # kish factor
-    if (len(w)==0):
-        return 0
-    res = np.sum(w)**2 / (len(w) * np.sum(w**2))
-    return res
-
-def efficiency_1(s1, s2, s_max): 
-    eff = f_kish(np.sign(s2)*np.maximum(1, np.abs(s2)/s_max)) * np.sum(s1) / (len(s1) * s_max)
-    return eff 
-
-def efficiency_2(x2, x3, x_max, s3, s_max):
-    eff = f_kish(np.sign(s3)*np.maximum(1, x3*np.maximum(1, np.abs(s3)/s_max)/x_max)) * np.sum(x2) / (len(x2) * x_max)
-    return eff
-
-"""
-def f_eff(w2, w3):                     # effective gain factor
-    t_ratio = [20, 50, 100, 500]       # t_standard / t_surrogate
-    eff_st = 0.1                       # standard efficiency
-""" 
-
-
-for i_r in range(len(arr_r)):                    # loop to test different maxima conditions
+for i_r1 in range(len(arr_r)):                    # loop to test different maxima conditions
 
     np.random.seed(seed_all)                     # each test has the same seed
 
     # first unweighting
     s1 = model.predict(X_val)
     s1 = s1.reshape(len(s1))
-    if (output=="logw"):
+    if (output=="lnw"):
         s1 = np.e**(-s1)                         # model predict -log(w)
+    r = arr_r[i_r1]                              # parameter of the maxima function for the first unwgt
     s_max = my_max(s1)
     rand1 = np.random.rand(len(s1))             
     w2 = np.empty(0)                             # real wgt evaluated after first unwgt
@@ -209,47 +232,78 @@ for i_r in range(len(arr_r)):                    # loop to test different maxima
             w2 = np.append(w2, wgt_val[i])
     x2 = np.divide(w2, np.abs(s2))
     
-    # second unweighting, based on the ratio x
-    rand2 = np.random.rand(len(x2))
-    if (maxfunc=="mqr"):
-        x_max = my_max(s2, x2)
-    if (maxfunc=="mmr"):
-        x_max = my_max(x2) 
-    s3 = np.empty(0)                             # wgt of kept event after second unweighting
-    x3 = np.empty(0)
-    for i in range(len(s2)):                     # second unweighting, based on the ratio x
-        if ((x2[i]*max(1, np.abs(s2[i])/s_max)/x_max) > rand2[i]):
-            s3 = np.append(s3, s2[i])
-            x3 = np.append(x3, x2[i])
-    
-    # data for plots
-    arr_eff1.append(efficiency_1(s1, s2, s_max))
-    arr_eff2.append(efficiency_2(x2, x3, x_max, s3, s_max))
-    arr_smax.append(s_max)
-    arr_xmax.append(x_max)
-    axs_eff[0].annotate(arr_r[i_r], xy=(arr_smax[i_r], arr_eff1[i_r]))
-    axs_eff[1].annotate(arr_r[i_r], (arr_xmax[i_r], arr_eff2[i_r]))    
-    if (arr_r[i_r]==0):
-        axs_eff[0].axvline(x=s_max, color='r')
-        axs_eff[1].axvline(x=x_max, color='r')
+    arr_eff1[i_r1] = efficiency_1(s1, s2, s_max)
+    arr_smax[i_r1] = s_max
+
+    for i_r2 in range(len(arr_r)):               # to test all combinations of s_max and x_max 
+        # second unweighting, based on the ratio x
+        rand2 = np.random.rand(len(x2))
+        r = arr_r[i_r2]                          # parameter of the maxima function for the second unwgt
+        if (maxfunc=="mqr"):
+            x_max = my_max(s2, x2)
+        if (maxfunc=="mmr"):
+            x_max = my_max(x2) 
+        s3 = np.empty(0)                         # wgt of kept event after second unweighting
+        x3 = np.empty(0)
+        for i in range(len(s2)):                 # second unweighting, based on the ratio x
+            if ((x2[i]*max(1, np.abs(s2[i])/s_max)/x_max) > rand2[i]):
+                s3 = np.append(s3, s2[i])
+                x3 = np.append(x3, x2[i])
+        
+        # data for plots
+        mtx_xmax[i_r1, i_r2] = x_max
+        mtx_eff2[i_r1, i_r2] = efficiency_2(x2, x3, x_max, s3, s_max)
+        #axs_eff[1].annotate(arr_r[i_r2], (x_max, mtx_eff2[i_r1, i_r2]))
+        mtx_feff[i_r1, i_r2] = f_eff( s3, x3, s_max, x_max, arr_eff1[i_r1], mtx_eff2[i_r1, i_r2])
 
 
 
 ##################################################
 # plot of results
 ##################################################
+cmap = plt.get_cmap('plasma')
+colors = cmap(np.linspace(0, 1, len(arr_r))) 
 axs_eff[0].plot(arr_smax, arr_eff1, marker='.')
-axs_eff[1].plot(arr_xmax, arr_eff2, marker='.')
-x1 = np.divide(wgt_val, s1)
+axs_eff[0].legend(loc='best')
+
+axs_eff[1].set_xlim([0, 12])
+for i_r1 in range(len(arr_r)):                   # to mark the values for max=real_max with larger points
+    #axs_eff[0].annotate(arr_r[i_r1], xy=(arr_smax[i_r1], arr_eff1[i_r1]))
+    if (arr_r[i_r1]==0):
+        axs_eff[0].scatter(arr_smax[i_r1], arr_eff1[i_r1], s= 50)
+    for i_r2 in range(len(arr_r)):
+        if (arr_r[i_r1]==0):
+            axs_eff[1].scatter(mtx_xmax[i_r1, i_r2], mtx_eff2[i_r1, i_r2], s= 50)
+        else:
+            if (arr_r[i_r2]==0):
+                axs_eff[1].scatter(mtx_xmax[i_r1, i_r2], mtx_eff2[i_r1, i_r2], s= 50)
+for i in range(len(arr_r)):                      # plot the curve of the efficiencies in function of the x_max with fixed s_max
+    axs_eff[1].plot(mtx_xmax[i, :], mtx_eff2[i], marker='.', color=colors[i], label="s_max = {:.3f}".format(arr_smax[i]))
+axs_eff[1].legend
+axs_eff[1].legend(loc='best')
+
+for i in range(len(arr_r)):                      # x, y labels of the f_eff colormap
+    axs_eff[2].text(0+i, -0.7 , "s_max: {:.3f} \nr: {}".format(arr_smax[i], arr_r[i]), fontsize = 7)
+    axs_eff[2].text(-1, 0+i , "x_max*: {:.3f} \nr: {}".format(mtx_xmax[i, i], arr_r[i]), fontsize = 7)
+#axs_eff[2].axis('off')
+axs_eff[2].set_yticklabels([])
+axs_eff[2].set_xticklabels([])
+h1 = axs_eff[2].pcolormesh(mtx_feff, cmap=cmap)
+plt.colorbar(h1, ax=axs_eff[2])
+
+x1 = np.abs(np.divide(wgt_val, s1))
 lin_bins1 = np.linspace(min(x1), max(x1), 10**3) 
 lin_bins2 = np.linspace(-0.5, 2.5, 50) 
 axs_ws[0].hist(x1, bins=lin_bins1)
 axs_ws[1].hist(x1, bins=lin_bins2)
 wbins = np.linspace(min(wgt_val), max(wgt_val), 50)
 xbins = np.linspace(0, 2, 20)
-h = axs_ws[2].hist2d(wgt_val, x1, bins=[wbins, xbins])
-plt.colorbar(h[3], ax= axs_ws[2]) 
-fig_eff.savefig("/home/lb_linux/nn_unwgt/plot_eff_{}_16_16_{}_{}_seed{}.pdf".format(maxfunc, norm, output, seed_all), format='pdf')
-fig_ws.savefig("/home/lb_linux/nn_unwgt/plot_ws_{}_16_16_{}_{}_seed{}.pdf".format(maxfunc, norm, output, seed_all), format='pdf')
+h2 = axs_ws[2].hist2d(wgt_val, x1, bins=[wbins, xbins])
+plt.colorbar(h2[3], ax= axs_ws[2]) 
+fig_eff.savefig("/home/lb_linux/nn_unwgt/plot_16_16_{}_{}_{}_{}_seed{}_eff.pdf".format(norm, lossfunc, output, maxfunc, seed_all), format='pdf')
+fig_ws.savefig("/home/lb_linux/nn_unwgt/plot_16_16_{}_{}_{}_{}_seed{}_ws.pdf".format(norm, lossfunc, output, maxfunc, seed_all), format='pdf')
 
 
+
+print("\n--------------------------------------------------")
+print(plot_legend)
